9ce48ee39593e5919050dae8f694ba438e50296e
michel vergeest <michelvergeest@trekwerk.com>
2014-11-17 08:51:42 +0100

    first commit

---
+++ DmxRx0.cpp
@@ -0,0 +1,23 @@
+#include <DmxRx0.h>
+
+// create instance 
+DmxRx0 Dmx0Receiver;
+
+ISR (SIG_USART0_RECV) {
+	Dmx0Receiver.receive(UCSR0A, UDR0);
+}
+
+// init and enable uart
+void DmxRx0::begin() {
+	
+	//call base class function
+	DmxRx::setMemory();
+
+	cli();
+	UBRR0 = DMXRX_BAUDRATE;
+	UCSR0A |= DMXRX_UCSRA;
+	UCSR0C |= DMXRX_UCSRC;  // 8N2
+	UCSR0B |= DMXRX_UCSRB;  // enable rx interrupts
+	sei();
+}
+
---
+++ DmxRx0.h
@@ -0,0 +1,22 @@
+/* DmxRx0 */
+#ifndef _DMXRX0_H
+#define _DMXRX0_H
+
+#include <DmxRx.h>
+
+extern "C" { 
+	void SIG_USART0_RECV (void) __attribute__((__always_inline__));
+}
+
+
+class DmxRx0 : public DmxRx {
+	
+	friend void SIG_USART0_RECV (void);
+   		
+	public:
+		void	begin();
+};
+
+extern DmxRx0 Dmx0Receiver;
+
+#endif // _DMXRX0_H
